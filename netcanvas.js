75bd438cf57a673753baeac415b09c0903a51dc2
kuna <kgdgwn98@gmail.com>
2014-07-04 01:10:09 +0900

    finally netcanvas working done!!

--- netcanvas.js
+++ netcanvas.js
@@ -2,18 +2,100 @@
  * netcanvas for croquis
  * by @lazykuna
  * for modified ver of croquis (need overlaylayer - croquis.overlay.js)
+ * prototype: onNick, onChat, onMessage, onJoin, onQuit, onError, onClose, onOpen, onEnter
  */
 
-var NetCanvas = function(url, croquis) {
+
+var NetCanvas = function(url, croquis, prototype) {
 	var self = this;
 	self.croquis = croquis;
+	self.nick = {};
+	self.isconnected = false;
+	self.reconnect = function () {
+		if ('WebSocket' in window) {
+			self.wSocket = new WebSocket(url);
+
+			// add websocket event handler
+			self.wSocket.onmessage = self.onmessage;
+			self.wSocket.onopen = self.onopen;
+			self.wSocket.onclose = self.onclose;
+			self.wSocket.onerror = self.onerror;
+
+			// add canvas onchange handler
+			self.croquis.addEventListener('onchanged', self.onchange);
+
+			console.log("Websocket connected");
+		} else {
+			console.log("this broswer doesnt support Websocket");
+		}
+	}
+
+	if (prototype != null) {
+		for (var p in prototype) {
+			self[p] = prototype[p];
+		}
+	}
+
+	function isNickExists(nick) {
+		for (var id in self.nick) {
+			if (self.nick[id] == nick)
+				return true;
+		}
+		return false;
+	}
+
+	function getNick(id) {
+		return self.nick[id];
+	}
+
+	function addNick(id, nick) {
+		self.nick[id] = nick;
+		console.log(id + " nicknamed as " + nick);
+	}
+
+	function removeNick(id) {
+		if (self.nick[id] != null)
+			delete self.nick[id];
+	}
+
+	self.sendData = function(data) {
+		// data should be consisted of [cmd], [data]
+		if (!self.isconnected) {
+			console.log("attempt to send data while its not connected");
+			console.log(data);
+			return;
+		}
+		self.wSocket.send(JSON.stringify(data));
+	}
 
 	self.onmessage = function(e) {
 		obj = JSON.parse(e.data);
 		if (obj["cmd"] == "JOIN") {
 			console.log(obj["name"] + " joined");
 			self.croquis.addOverlayLayer(obj["name"]);
+			if (self.onJoin)
+				self.onJoin(obj["name"]);
+		} else if (obj["cmd"] == "SIZE") {
+			console.log("size cmd - width " + obj["width"] + ", height " + obj["height"]);
+			self.croquis.setCanvasSize(obj["width"], obj["height"]);
+		} else if (obj["cmd"] == "NICK") {
+			addNick(obj["name"], obj["nick"])
+			if (self.onNick)
+				self.onNick(obj["name"], obj["nick"]);
+		} else if (obj["cmd"] == "MSG") {
+			console.log("message from server - " + obj["msg"]);
+			if (self.onMessage)
+				self.onMessage(obj["msg"]);
+		} else if (obj["cmd"] == "CHAT") {
+			var nick = getNick(obj["name"]);
+			if (nick == undefined)
+				nick = obj["name"];
+			console.log("message - " + nick + ", " + obj["msg"]);
+			if (self.onChat)
+				self.onChat(nick, obj["msg"]);
 		} else if (obj["cmd"] == "IMG") {
+			if (obj["data"] == "")
+				return;
 			console.log("new img from " + obj["name"]);
 			var img = new Image();
 			img.onload = function () {
@@ -24,45 +106,41 @@ var NetCanvas = function(url, croquis) {
 			}
 			img.src = obj["data"];
 		} else if (obj["cmd"] == "QUIT") {
-			console.log(obj["name"] + " exited");
+			var nick = getNick(obj["name"]);
+			if (nick == undefined)
+				nick = obj["name"];
+			console.log(nick + " exited");
 			self.croquis.removeOverlayLayer(obj["name"]);
+			if (self.onQuit)
+				self.onQuit(nick);
+		} else if (obj["cmd"] == "ENTER") {
+			console.log("joined!");
+			console.log(obj);
+			if (self.onEnter)
+				self.onEnter(obj["roomname"], obj["width"], obj["height"]);
 		}
 	};
 
 	self.onopen = function(e) {
-
+		console.log("connected!");
+		self.isconnected = true;
+		if (self.onOpen)
+			self.onOpen();
 	};
 
 	self.onclose = function(e) {
 		// server disconnection
 		console.log("connection off.");
+		self.isconnected = false;
+		if (self.onClose)
+			self.onClose();
 	};
 
 	self.onchange = function(e) {
 		var canvas = self.croquis.getLayerCanvas(0);
 		var data = canvas.toDataURL();	// PNG
-		self.wSocket.send(JSON.stringify({"cmd":"IMG", "data":data}));
+		self.sendData({"cmd":"IMG", "data":data});
 	};
 
-	self.Disconnect = function() {
-		// TODO
-		//
-		self.wSocket.close();
-	};
-
-	if ('WebSocket' in window) {
-		self.wSocket = new WebSocket(url);
-
-		// add websocket event handler
-		self.wSocket.onmessage = self.onmessage;
-		self.wSocket.onopen = self.onopen;
-		self.wSocket.onclose = self.onclose;
-
-		// add canvas onchange handler
-		self.croquis.addEventListener('onchange', self.onchange);
-
-		console.log("Websocket connected");
-	} else {
-		console.log("this broswer doesnt support Websocket");
-	}
+	self.reconnect();
 }
--- server.py
+++ server.py
@@ -11,23 +11,87 @@ class IndexHandler(web.RequestHandler):
 		self.render("netcanvas.htm")
 
 # logined users
+# each element consists -
+# (connection, name, nick, image)
 cl = []
+
+def getConnFromClient(c):
+	return c[0]
+
+def getNameFromClient(c):
+	return c[1]
+
+def getNickFromClient(c):
+	return c[2]
+
+def getImageFromClient(c):
+	return c[3]
+
+# rooms
+# each room has joined client's (name)
+# room information(consists) - (roomname, canvaswidth, canvasheight, maxperson, [names])
+rooms = []
+
+def roomExists(roomname):
+	return (getRoom(roomname) != None)
+
+def getRoom(roomname):
+	for r in rooms:
+		if (r[0] == roomname):
+			return r
+	return None
+
+def roomCreate(roomname, width, height):
+	try:
+		tw = int(width)
+		th = int(height)
+
+		if (tw < 0 or th < 0):
+			return False
+
+		if (tw > 1280 or th > 800):
+			return False
+
+		rooms.append([roomname, width, height, 5, []])
+		return True
+	except:
+		return False
+
+def getRoomnameFromName(name):
+	for r in rooms:
+		names = r[4]
+		for n in names:
+			if (n == name):
+				return r[0]
+	return None
+
+def getClientsFromRoomname(roomname):
+	ret = []
+	for r in rooms:
+		if (len(ret) > 0):
+			break
+		if (r[0] == roomname):
+			names = r[4]
+			for c in cl:
+				if (c[1] in names):
+					ret.append(c)
+	return ret
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
 
-def informToAll(data):
-	for c in cl:
+def informToAll(roomname, data):
+	rcl = getClientsFromRoomname(roomname)
+	for c in rcl:
 		c[0].write_message(json.dumps(data))
 
 def informToTarget(data, target):
-	for c in cl:
-		if (c[0] == target):
-			c[0].write_message(json.dumps(data))
-			break
+	#print data
+	target.write_message(json.dumps(data))
 
-def informToAllExcept(data, exception):
-	for c in cl:
+def informToAllExcept(roomname, data, exception):
+	rcl = getClientsFromRoomname(roomname)
+	for c in rcl:
 		if (c[0] != exception):
 			c[0].write_message(json.dumps(data))
 
@@ -42,38 +106,135 @@ def quitConnection(obj):
 	if (target == None):
 		return
 
-	informToAllExcept({'cmd':"QUIT", 'name':target[1]}, target[0])
+	name = target[1]
+	roomname = getRoomnameFromName(name)
+	if (roomname != None):
+		# inform to all about exiting
+		informToAllExcept(roomname, {'cmd':"QUIT", 'name':name}, target[0])
+		# reset room info (automatically exit)
+		i = 0
+		for r in rooms:
+			if (name in r[4]):
+				r[4].remove(name)
+				#rooms[i] = r
+				if (len(r[4]) == 0):
+					del rooms[i]
+					i -= 1
+			i += 1
+
+	# remove from client info
 	cl = [c for c in cl if c[0] != obj]
+
 
 class SocketHandler(websocket.WebSocketHandler):
 	def open(self):
 		if self not in cl:
 			newid = id_generator()
-			informToAll({'cmd':"JOIN", 'name':newid})
-			cl.append([self, newid])
-			for c in cl:
-				if (c[0] != self):
-					informToTarget({'cmd':"JOIN", 'name':c[1]}, self)	# inform all joins
+			cl.append([self, newid, None, None])
 			print "new client from", self
 
 	def on_message(self, data):
-		ret = json.loads(data)
-		if (ret['cmd'] == "IMG"):
-			# find id of sender
-			senderid = None
-			for c in cl:
-				if (c[0] == self):
-					senderid = c[1]
-			if (senderid == None):
-				print "invalid data from", self
-				return
+		# find id of sender
+		senderid = None
+		sender = None
+		for c in cl:
+			if (c[0] == self):
+				senderid = c[1]
+				sender = c
+		if (senderid == None):
+			print "invalid data from ", self
+			return
 
-			informToAllExcept({'cmd':"IMG", 'name':senderid, 'data':ret['data']}, self)
+		ret = json.loads(data);
+		if (ret['cmd'] == "IMG"):
+			# get room name
+			roomname = getRoomnameFromName(senderid)
+			if (roomname == None):
+				print "IMG error - no roomname"
+				return
+			# inform IMG everybody except sender own
+			informToAllExcept(roomname, {'cmd':"IMG", 'name':senderid, 'data':ret['data']}, self)
+			# save img data
+			sender[3] = ret['data']
 			print "received IMG data from", self
+		elif (ret['cmd'] == "NICK"):
+			# get room name
+			roomname = getRoomnameFromName(senderid)
+			if (roomname == None):
+				print "NICK error - no roomname"
+				return
+			# inform NICK everybody except sender own
+			informToAllExcept(roomname, {'cmd':"NICK", 'name':senderid, 'nick':ret['nick']}, self)
+			# save nick data
+			sender[2] = ret['nick']
+			print (self, " changed nick to ", ret['nick'])
+		elif (ret['cmd'] == "CHAT"):
+			# get room name
+			roomname = getRoomnameFromName(senderid)
+			if (roomname == None):
+				print "CHAT error - no roomname"
+				return
+			# inform CHAT everybody except sender own
+			informToAllExcept(roomname, {'cmd':"CHAT", 'name':senderid, 'msg':ret['msg']}, self)
+		elif (ret['cmd'] == "ENTER"):
+			# TODO: is room exists?
+			r = getRoom(ret['roomname'])
+			if (r == None):
+				informToTarget({'cmd':'MSG', 'msg':'no room such name!'}, self)
+				return
+			# TODO: if over maxpeople, send "full capacity" message.
+			if (len(r[4]) == r[3]):
+				informToTarget({'cmd':'MSG', 'msg':'full room!'}, self)
+				return
+			# TODO: inform join to self
+			print "ENTER", sender
+			self.joinRoom(sender, r)
+		elif (ret['cmd'] == "CREATE"):
+			# TODO: is room exists?
+			if (roomExists(ret['roomname'])):
+				informToTarget({'cmd':'MSG', 'msg':'already exists!'}, self)
+				return
+			# TODO: if not exists, then create
+			if (not roomCreate(ret['roomname'], ret['width'], ret['height'])):
+				informToTarget({'cmd':'MSG', 'msg':'error occured during creating room'}, self)
+				return
+			# TODO: inform join to self
+			r = getRoom(ret['roomname'])
+			self.joinRoom(sender, r)
 
 	def on_close(self):
 		print "closed connection from", self
 		quitConnection(self)
+
+	def joinRoom(self, c, room):
+		# inform to everybody that I joined except me
+		print c
+		informToAllExcept(room[0], {'cmd':"JOIN", 'name':c[1]}, c[0])
+
+		# get information of room (canvas size)
+		print c
+		informToTarget({'cmd':"ENTER", 'roomname':room[0], 'width':room[1], 'height':room[2]}, c[0])
+		rname = room[4]
+		rcl = []
+		for c__ in cl:
+			if (c__[1] in rname):
+				rcl.append(c__)
+		# get information of everybody about join
+		for c_ in rcl:
+			if (c_[0] != c[0]):
+				informToTarget({'cmd':"JOIN", 'name':c_[1]}, c[0])
+		# get information of everybody about nick
+		for c_ in rcl:
+			if (c_[2] != None):
+				informToTarget({'cmd':"NICK", 'name':c_[1], 'nick':c_[2]}, c[0])
+		# get information of everybody about img
+		for c_ in rcl:
+			if (c_[3] != None):
+				informToTarget({'cmd':"IMG", 'name':c_[1], 'data':c_[3]}, c[0])
+
+		# add me into the room!
+		room[4].append(getNameFromClient(c));
+		print "client ", self, " joined ", room[0]
 
 def main():
 	app = web.Application([
